c36a784642d7ab1fef1b422cf618dce37bc221e5
inferknob <137335926+inferknob@users.noreply.github.com>
2024-10-24 18:46:40 -0400

    Update dialog.lua

--- files/content/payphone/content/dialog.lua
+++ files/content/payphone/content/dialog.lua
@@ -63,5 +63,101 @@ return {
 				end,
 			},
 		},
+	},
+
+	name = "Microsoft Tech Support",
+		portrait = "mods/noita.fairmod/files/content/payphone/portrait_blank.png",
+		typing_sound = "default",
+		text = [Hello Microsoft Tech Support what is your problem?]],
+		options = {
+			{
+				text = "My machine isn't working!",
+				func = function(dialog)
+					dialog.show({
+						text = [[I see, please open up command prompt please.
+						options = {
+							{
+								text = "Ok",
+								func = function(dialog)
+									dialog.show({
+										text = [[Now type in: transferallmoneytototallyrealmicrosoft()
+										options = {
+										{
+											text = "Ok",
+											func = function(dialog)
+												dialog.show({
+													text = [HAHAHA DUMBASS.{@func disconnected}
+													(They hung up...)]],
+													options = {
+														{
+															text = "..."
+															func = function(dialog)
+																hangup()
+															end,
+														},
+													end,
+												})
+											end,
+										},
+														
+										text = "Nah",
+										func = function(dialog)
+											hangup()
+										end,
+										},
+									})
+								end,
+							},
+							
+							text = "Nah",
+								func = function(dialog)
+									hangup()
+								end,
+							},
+						},
+					})
+				end,
+			},
+			{
+				text = "Who is this?",
+				func = function(dialog)
+					dialog.show({
+						text = [[{@func disconnected}
+						(They hung up...)]],
+						options = {
+							{
+								text = "...",
+								func = function(dialog)
+									hangup()
+								end,
+							},
+						},
+					})
+				end,
+			},
+			{
+				text = "You called me?",
+				func = function(dialog)
+					dialog.show({
+						text = [[{@func disconnected}
+						(They hung up...)]],
+						options = {
+							{
+								text = "...",
+								func = function(dialog)
+									hangup()
+								end,
+							},
+						},
+					})
+				end,
+			},
+			{
+				text = "Goodbye.",
+				func = function(dialog)
+					hangup()
+				end,
+			},
+		},
 	}
 }
